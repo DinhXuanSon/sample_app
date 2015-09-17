e61917c64a74c1ab2b5ce5430c8fc4b4b5c9b828
DinhXuanSon <dinh.xuan.son@framgia.com>
2015-09-17 16:49:42 +0700

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
@@ -46,7 +47,6 @@ class User < ActiveRecord::Base
     update_attribute(:activated,    true)
     update_attribute(:activated_at, Time.zone.now)
   end
-
   # Sends activation email.
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
@@ -66,6 +66,12 @@ class User < ActiveRecord::Base
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
+  # Defines a proto-feed.
+  # See "Following users" for the full implementation.
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+
   private
 
     # Converts email to all lower-case.
@@ -78,5 +84,4 @@ class User < ActiveRecord::Base
       self.activation_token  = User.new_token
       self.activation_digest = User.digest(activation_token)
     end
-  
 end
